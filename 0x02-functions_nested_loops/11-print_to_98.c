5d9e7d40d53f6fbc8a6293e873d0c1cbc75eb9a6
Uchechi-Ogwo <ogwouchechi@outlook.com>
2023-04-26 12:01:42 -0700

    Learn your times table

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print to 98
+ * @n: number to start from
+ * Return: 0 or 1
+ */
+
+void print_to_98(int n)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,7 @@ void times_table(void)
 			}
 			else if (rep < 10)
 			{
-				_putchar(',');
+				_putchar(' ');
 				_putchar('0' + rep);
 			}
 			else
